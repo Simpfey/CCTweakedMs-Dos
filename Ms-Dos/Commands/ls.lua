4022c9b8c854440d0152609ecbff901e47935512
Simpfey <95985392+Simpfey@users.noreply.github.com>
2025-02-07 20:57:44 +0100

    Update ls.lua

--- Ms-Dos/Commands/ls.lua
+++ Ms-Dos/Commands/ls.lua
@@ -3,12 +3,27 @@ local cwd = Fcwd.readAll():gsub("\n*$","")
 Fcwd.close()
 
 local files = fs.list(cwd)
+
+local organized = {}
+
 for i=1,#files do
     if fs.isDir(cwd.."/"..files[i]) then
+        organized[#organized+1] = files[i]
+    end
+end
+
+for i=1,#files do
+    if not fs.isDir(cwd.."/"..files[i]) then
+        organized[#organized+1] = files[i]
+    end
+end
+
+for i=1,#organized do
+    if fs.isDir(cwd.."/"..organized[i]) then
         term.setTextColor(colors.green)
     end
     
-    print(files[i])
+    print(organized[i])
     
     term.setTextColor(colors.white)
 end
